c99a59c46c69f73722daa12d7d5679007eee4cdd
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2026-02-12 08:26:03 +0100

    initial commit with nav and hero section

---
+++ components/Hero.jsx
@@ -0,0 +1,30 @@
+
+const Hero = () => {
+  return (
+    <section className="w-full px-4 md:px-6 bg-gray-50 flex items-center justify-center py-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+            <div>
+                <h1 className="text-5xl font-bold mb-4 capitalize">
+                    Grow your business with our {" "}
+                    <span className="text-blue-600">digital solutions.</span>
+                </h1> 
+                <p className="text-md mb-6 md:w-[90%] w-full">We help you achieve your goals with tailored digital marketing strategies. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, accusantium.</p>
+                <div className="flex space-x-4 items-center">
+                    <button className="px-6 py-2 bg-blue-800 text-sm text-white rounded-md hover:bg-blue-900 transition-all">
+                        Get Started
+                    </button>
+                    <button className="px-6 py-2 bg-gray-300 border border-gray-600 text-gray-800 text-sm rounded-md hover:bg-gray-600 hover:text-white transition-all">
+                        View Our Work
+                    </button>
+                </div>
+            </div>
+            
+            <div>
+                <img src="/hero.jpg" alt="hero image" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { FaBars } from "react-icons/fa";
+
+const navLinks = [
+    {
+        id: 1,
+        label: "Home"
+    },
+    {
+        id: 2,
+        label: "Services"
+    },
+    {
+        id: 3,
+        label: "Portfolio"
+    },
+    {
+        id: 4,
+        label: "About"
+    },
+    {
+        id: 5,
+        label: "Contact"
+    },
+]
+
+const Navbar = () => {
+  return (
+    <nav className="px-4 md:px-6 bg-white">
+        <div className="flex items-center justify-between p-3">
+            <div className="flex items-center gap-2">
+                <img src="/logo.jpg" alt="project image" className="w-9 h-9" />
+                <span className="md:text-3xl text-xl font-semibold text-gray-700">AgencyPro</span>
+            </div>
+            
+            {/* NavLinks */}
+            <ul className="hidden md:flex gap-5">
+              {navLinks.map((link) => (
+                    <li 
+                        key={link.id} 
+                        className="font-medium text-gray-500">
+                            {link.label}
+                    </li>
+              ))} 
+            </ul>
+            
+            <div className="hidden md:block">
+                <button className="px-6 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-900 transition-all">Get A Quote</button>
+            </div>
+            
+            {/* Mobile menu */}
+            <div className="md:hidden">
+                <FaBars className="w-7 h-7" />
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
